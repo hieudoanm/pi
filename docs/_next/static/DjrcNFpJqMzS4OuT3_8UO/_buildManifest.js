e3bc57945b56714cb0eee699888198f9ae7058d8
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-12-25 00:40:06 +0700

    remove pi

---
+++ docs/_next/static/DjrcNFpJqMzS4OuT3_8UO/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/d882fad56262dfec.js"
+  ],
+  "/_error": [
+    "static/chunks/bbbde53314311a78.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/bbbde53314311a78.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/99cfdf3a5d8f6b2f.js",
+  "static/chunks/1ca5cf07e7c49045.js",
+  "static/chunks/turbopack-64a9f20166e562d3.js"
+])
